32806139f446152e1d1d29b4ddec862ce85331ca
achtundneun <80224522+achtundneun@users.noreply.github.com>
2021-06-22 15:32:35 +0300

    Add files via upload

---
+++ CalculatorApp2/Math.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorApp2
+{
+        public class Math
+        {
+            public static double Add(double numberOne, double numberTwo)
+            {
+                return numberOne + numberTwo;
+            }
+
+            public static double Minus(double numberOne, double numberTwo)
+            {
+                return numberOne - numberTwo;
+            }
+            public static double Multiple(double numberOne, double numberTwo)
+            {
+                return numberOne * numberTwo;
+            }
+            public static double Divide(double numberOne, double numberTwo)
+            {
+                return numberOne / numberTwo;
+            }
+        }
+}
+
